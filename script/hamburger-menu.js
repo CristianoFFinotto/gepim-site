c998a2d312901859efc27938aa4adb302bd8786b
cristiano francesco finotto <cristiano.finotto@edu.itspiemonte.it>
2022-08-14 18:38:23 +0200

    optimization v-1.0

--- script/hamburger-menu.js
+++ script/hamburger-menu.js
@@ -3,24 +3,22 @@ const hamburger = document.querySelector('.hamburger');
 const header = document.querySelector('.header');
 const navList = document.querySelectorAll('.navbar__list');
 const content = document.querySelector('#content');
-const contattaci = document.querySelector('#contattaci');
+const navLink = document.querySelectorAll('nav li a');
 
 
 hamburger.addEventListener('click', () => {
     header.classList.toggle('header-toggle-event')
     hamburger.classList.toggle('is-active');
     navList[0].classList.toggle('d-none');
+    content.classList.toggle('d-none');
 });
 
-contattaci.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    header.classList.toggle('header-toggle-event')
-    hamburger.classList.toggle('is-active');
-    navList[0].classList.toggle('d-none');
-
-    window.scroll({
-        top: window.document.body.clientHeight, behavior: 'smooth'
+for (let index = 0; index < navLink.length; index++) {
+    navLink[index].addEventListener('click', () => {
+        header.classList.toggle('header-toggle-event')
+        hamburger.classList.toggle('is-active');
+        navList[0].classList.toggle('d-none');
+        content.classList.toggle('d-none');
     });
-});
+}
 
